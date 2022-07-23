a7ca9b660680c63f3c0f481d0ddda3ab3d9c7296
Göksenin Güngör <goksenin.gungor@wunder.is>
2022-07-23 08:42:13 +0300

    ci: eslint test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'react-native-wcandillon'],
+  extends: ['@react-native-community', 'prettier'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'reanimated'],
+  plugins: ['@typescript-eslint', 'reanimated', 'react-native'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: './',
+    ecmaFeatures: {
+      jsx: false,
+      tsx: true,
+    },
   },
   overrides: [
     {
@@ -29,5 +33,14 @@ module.exports = {
   ],
   rules: {
     'reanimated/js-function-in-worklet': 2,
+    'global-require': 'off',
+    'react/jsx-filename-extension': ['error', {extensions: ['.ts', '.tsx']}],
+    'no-use-before-define': [
+      'error',
+      {functions: true, classes: true, variables: false},
+    ],
+    'react-native/no-unused-styles': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
   },
 };
